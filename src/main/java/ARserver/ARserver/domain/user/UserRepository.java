022fb01f692fd09e55342ca11cd239af3e42abd5
leejaehun <ldyjka@gmail.com>
2023-11-04 21:11:26 +0900

    fix: id 재생성

--- src/main/java/ARserver/ARserver/domain/user/UserRepository.java
+++ src/main/java/ARserver/ARserver/domain/user/UserRepository.java
@@ -4,9 +4,21 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class UserRepository {
+    private static UserRepository instance;
     private static final Map<String, User> store = new ConcurrentHashMap<>();
+    private UserRepository(){}
+
+    private Long sequence = 1L;
+
+    public static UserRepository getInstance(){
+        if(instance == null){
+            instance = new UserRepository();
+        }
+        return instance;
+    }
 
     public User save(User user){
+        user.setId(sequence++);
         store.put(user.getEmail(), user);
         return user;
     }
