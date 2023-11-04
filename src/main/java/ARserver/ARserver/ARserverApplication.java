d09eb1c8c9883264bdf7410465c4f0419cf09fed
leejaehun <ldyjka@gmail.com>
2023-11-04 20:47:54 +0900

    Initial commit

---
+++ src/main/java/ARserver/ARserver/ARserverApplication.java
@@ -0,0 +1,13 @@
+package ARserver.ARserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ARserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ARserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ARserver/ARserver/ARserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ARserver.ARserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ARserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
