11fe9f19c11769aa6258326ae27213e5e879a2e0
leejaehun <ldyjka@gmail.com>
2023-11-04 20:51:39 +0900

    feat: User domain class 추가

---
+++ src/main/java/ARserver/ARserver/domain/user/User.java
@@ -0,0 +1,11 @@
+package ARserver.ARserver.domain.user;
+
+public class User {
+    private String id;
+    private String password;
+
+    public User(String id, String password){
+        this.id = id;
+        this.password = password;
+    }
+}
